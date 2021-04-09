53cee789e2a1844766ba7d273c1f9c90e105d701
Leonardo Gonçalves <leonardo@lginfo.com.br>
2021-04-09 00:20:18 -0300

    Exercícios e Desafios Cousera

---
+++ Coursera/semana_3/teste.py
@@ -0,0 +1,18 @@
+import math
+x = 10
+y = 15
+z = 25
+print(x == z - y and z != y - x or not y != z - x)
+
+print(x <= 30 and y >= 5)
+print(not y < 10 or not z == 10) 
+print(not y > 10 or not z > 10)
+
+
+a = 10
+if(a>=100):
+    print("maior")
+else:
+    print("maior2")    
+
+print(math.sqrt(-1))
